e32e8c9533693dbc6c36438c2270582aada90f15
Nader Shafik <nadermohamedshafik@hotmail.com>
2025-12-27 14:52:39 +0200

    admin

--- models/BouquetTemplate.js
+++ models/BouquetTemplate.js
@@ -3,12 +3,20 @@ import mongoose from "mongoose";
 const BouquetTemplateSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    description: String,
-    imageUrl: String,
-    price: { type: Number, required: true },
+    shape: String,
+    size: String,
+    slotCount: Number,
+    slots: [
+      {
+        index: Number,
+        x: Number,
+        y: Number,
+      },
+    ],
     active: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
+
 
 export default mongoose.model("BouquetTemplate", BouquetTemplateSchema);
